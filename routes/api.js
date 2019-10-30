ec2d68490ac0c1a01ff630cbc14f8b50a5f126d0
Glitch (foremost-catboat) <none>
2019-10-30 23:18:16 +0000

    🚧👂 Checkpoint ./routes/api.js:6684026/1

--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,7 @@ var expect = require('chai').expect;
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
     
   app.route('/api/replies/:board');
 
