e21c01eeb3180a30fb03ef01d6774849f40626bb
Glitch (foremost-catboat) <none>
2019-10-31 01:21:20 +0000

    🛫🐪 Checkpoint ./routes/api.js:6684026/705 ./package.json:6684026/1602 ./thread.js:6684026/283

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,36 @@
-/*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
-
 'use strict';
 
 var expect = require('chai').expect;
+const mongoose = require('mongoose');
 
 module.exports = function (app) {
   
   app.route('/api/threads/:board')
+  .get((req, res, next) => {
+    let board = req.params.board;
+  })
+  .post((req, res, next) => {
+    let board = req.params.board;
+  })
+  .put((req, res, next) => {
+    let board = req.params.board;
+  })
+  .delete((req, res, next) => {
+    let board = req.params.board;
+  })
     
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board')
+  .get((req, res, next) => {
+    let board = req.params.board;
+  })
+  .post((req, res, next) => {
+    let board = req.params.board;
+  })
+  .put((req, res, next) => {
+    let board = req.params.board;
+  })
+  .delete((req, res, next) => {
+    let board = req.params.board;
+  })
 
 };
