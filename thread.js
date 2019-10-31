5355ec6a4b303a06076e15bebe9937cd85ed9d77
Glitch (foremost-catboat) <none>
2019-10-31 01:33:16 +0000

    🌟🍲 Checkpoint ./package.json:6684026/1702 ./thread.js:6684026/535

--- thread.js
+++ thread.js
@@ -1,11 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
+const saltWorkFactor = 10;
 
 const threadSchema = new Schema({
-  text: {
-    type: String,
-    required: true
-  }
+  text: {type: String, required: true},
+  reported: {type: Boolean, default: false},
+  delete_password: {type: String, required: true},
+  replies: {type: [String]}
+  
 },{
   timestamps: true
+})
+
+threadSchema.pre('save', function next(){
+  var user = this;
+  if(!user.isModified('delete_password')){
+    return next();
+  }
+  bcrypt.genSalt(saltWorkFactor, (err, salt) => {
+    if(err){
+      return next(err)
+    }
+    brcypt.hash()
+  })
 })
